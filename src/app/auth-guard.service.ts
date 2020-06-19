1be9fe8b63056de18a19453697fd0c4d8656578a
guillaume <guillaumechaulet@hotmail.fr>
2020-06-19 10:48:18 +0200

    guard service routing first part

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import { AuthService } from "./auth.service";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService) {}
+
+    canActivate(route: ActivatedRouteSnapshot, 
+                state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
+
+                }
+
+}
